83f5a8c0af14144fee5ace1db0b1c781f1e00d97
Joseph Moukarzel <joseph.moukarzel@mobile-devices.fr>
2023-03-01 22:43:02 +0100

    Add main tetsting for register and elect

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ import (
 	"github.com/go-zookeeper/zk"
 	"github.com/rs/zerolog/log"
 	"gitlab.mobile-intra.com/cloud-ops/distributed-algorithms/election"
+	"gitlab.mobile-intra.com/cloud-ops/distributed-algorithms/registration"
 )
 
 func main() {
+	register()
+}
+
+func elect() {
 	leaderElection := &election.LeaderElection{
 		ZkNamespace: "/election",
 		ZkTimeout:   time.Second * 5,
@@ -44,4 +49,42 @@ func main() {
 			log.Info().Bool("isLeader", leaderElection.IsLeader).Msg("Is leader")
 		}
 	}
+}
+
+func register() {
+	// register with zookeeper
+	registerWithZookeeper := &registration.RegisterWithZookeeper{
+		ZkNamespace: "/registration",
+		ZkTimeout:   time.Second * 5,
+		Zookeepers:  []string{"127.0.0.1:2181", "127.0.0.1:12181", "127.0.0.1:22181"},
+		Data:        []byte("test"),
+		Log:         &log.Logger,
+	}
+	//connect and create namespace
+	conn, _, err := zk.Connect(registerWithZookeeper.Zookeepers, registerWithZookeeper.ZkTimeout)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	_, err = conn.Create(registerWithZookeeper.ZkNamespace, []byte{}, 0, zk.WorldACL(zk.PermAll))
+	if err != nil && err != zk.ErrNodeExists {
+		panic(err)
+	}
+	registerWithZookeeper.RegisterWithoutFailureRetries()
+	ticker := time.NewTicker(time.Second * 5)
+	tickerCancel := time.NewTicker(time.Second * 20)
+	go func() {
+		<-tickerCancel.C
+		registerWithZookeeper.Cancel()
+	}()
+	for {
+		select {
+		case <-registerWithZookeeper.Ctx.Done():
+			log.Info().Msg("Done")
+			return
+		case <-ticker.C:
+			log.Info().Bool("isRegistered", registerWithZookeeper.IsRegistered).Msg("Is registered")
+		}
+	}
+
 }
